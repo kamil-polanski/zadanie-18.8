3811c991061448bee4e148322f5849970e8cdc61
Kamil Polanski <polanski88@gmail.com>
2019-07-13 20:41:34 +0200

    Add gif.js

---
+++ components/gif.js
@@ -0,0 +1,22 @@
+const GIPHY_LOADING_URL = 'http://www.ifmo.ru/images/loader.gif';
+const styles = {
+  minHeight: '310px',
+  margin: '0.5em'
+};
+
+class Gif extends React.component {
+    geturl() {
+        return this.props.sourceUrl || GIPHY_LOADING_URL;
+    }
+    render() {
+        const url = this.props.loading ? GIPHY_LOADING_URL : this.props.url;
+        return (
+            <div style={styles}>
+              <a href={this.getUrl()} title='view this on giphy' target='new'>
+                <img id='gif' src={url} style={{width: '100%', maxWidth: '350px'}}/>
+              </a>
+            </div>
+          );
+    }
+
+}
