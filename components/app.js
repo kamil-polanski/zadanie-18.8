cf4d25ebea0e05ec7b19d79c799d98fff9f3ab96
Kamil Polanski <polanski88@gmail.com>
2019-07-22 21:15:57 +0200

    Add promise

--- components/app.js
+++ components/app.js
@@ -21,24 +21,46 @@ class App extends React.Component {
             this.props.onSearch(this.state.searchingText);
         }
     }
+
+
+    getGif (searchingText)  {  
+        const GIPHY_API_URL = 'https://api.giphy.com';
+        const GIPHY_PUB_KEY = '097ZgDDfAyrqjnu6orPBhkwwB7uv517y';
+        const url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open('GET', url);
+            xhr.onload = function() {
+                if (this.status === 200) {
+                        const data = JSON.parse(xhr.responseText).data; 
+                        const gif = {  
+                        url: data.fixed_width_downsampled_url,
+                        sourceUrl: data.url
+                    };
+                resolve(gif);  
+                } else {
+                reject('Wystąpił błąd!');
+                }
+            }
+            xhr.send();
+        })
+    }  
+
     handleSearch = (searchingText) => {  
         this.setState({
             loading: true 
         });
-        this.getGif(searchingText, function(gif) { 
-            this.setState({  
-                loading: false, 
-                gif: gif,  
-                searchingText: searchingText 
-            });
-        }.bind(this));
+        this.getGif(searchingText) 
+            .then(gif =>
+                this.setState({
+                    loading: false,
+                    gif: gif,
+                    searchingText: searchingText
+                })
+            )
+            .catch(err => console.error(err));
     }
-
-    getGif (searchingText, callback)  {  
-        const GIPHY_API_URL = 'https://api.giphy.com';
-        const GIPHY_PUB_KEY = '097ZgDDfAyrqjnu6orPBhkwwB7uv517y';
-        const url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  
-        const xhr = new XMLHttpRequest();  
+    /*
         xhr.open('GET', url);
         xhr.onload = function() {
             if (xhr.status === 200) {
@@ -52,7 +74,7 @@ class App extends React.Component {
         };
         xhr.send();
     }
-  
+  */
       
     render () {
         const styles = {
